6830c10aa0a8e17ae8431024aef5be403c236124
John Michael <jmichael@coverys.com>
2022-09-09 16:48:45 -0500

    first commit

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Validation algorithm
+
+    static Boolean IsUppercase(string s){
+
+        return s.All(char.IsUpper);
+    }
+    Console.WriteLine(IsUppercase("y"));
+
